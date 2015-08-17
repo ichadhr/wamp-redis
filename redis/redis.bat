96272a5bb6ae5ff694c5990eae5efd657004b496
ichadhr <ichadhr@gmail.com>
2015-08-17 19:44:42 +0700

    improve redis.bat

--- redis/redis.bat
+++ redis/redis.bat
@@ -10,13 +10,13 @@ echo  ^|  \  ^|      ^|    ^| ^|      ^|
 echo  /   \ /----/ /---/  / \___.'
 echo.
 echo ...............................................
-echo PRESS 1, 2 OR 3 to select your task, or 4 to EXIT.
+echo PRESS 1, 2, 3 OR 4 to select your task, or 5 to EXIT.
 echo ...............................................
 echo.
 echo 1 - Install Redis Service
 echo 2 - Start Redis
 echo 3 - Stop Redis
-echo 4 - Uninstall Redis
+echo 4 - Uninstall Redis Service
 echo 5 - EXIT
 echo.
 SET /P M=Type 1, 2, 3, 4, or 5 then press ENTER:
@@ -27,28 +27,73 @@ IF %M%==4 GOTO UNINSTALL
 IF %M%==5 GOTO EOF
 
 :INSTALL
+SC QUERY redis > NUL
+IF ERRORLEVEL 1060 (
 cd /d %~dp0
 redis-server --service-install redis.windows.conf --loglevel verbose
 ping 192.0.2.2 -n 1 -w 3000 > nul
 GOTO MAIN
+) else (
+echo # Redis service already installed.
+ping 192.0.2.2 -n 1 -w 1000 > nul
+echo Aborted.
+ping 192.0.2.2 -n 1 -w 1000 > nul
+GOTO MAIN
+)
 
 :REDSTART
-cd /d %~dp0
-redis-server --service-start
-ping 192.0.2.2 -n 1 -w 3000 > nul
+tasklist /FI "IMAGENAME eq redis-server.exe" 2> nul | find /I /N "redis-server.exe" > nul
+IF "%ERRORLEVEL%"=="0" (
+echo # Redis already started.
+ping 192.0.2.2 -n 1 -w 2000 > nul
+echo Aborted.
+ping 192.0.2.2 -n 1 -w 1000 > nul
 GOTO MAIN
+) else (
+    SC QUERY redis > NUL
+    IF ERRORLEVEL 1060 (
+    echo # You must first install Redis service.
+    ping 192.0.2.2 -n 1 -w 2000 > nul
+    echo Aborted.
+    ping 192.0.2.2 -n 1 -w 1000 > nul
+    GOTO MAIN
+    ) else (
+    cd /d %~dp0
+    redis-server --service-start
+    ping 192.0.2.2 -n 1 -w 3000 > nul
+    GOTO MAIN
+    )
+)
 
 :REDSTOP
+tasklist /FI "IMAGENAME eq redis-server.exe" 2> nul | find /I /N "redis-server.exe" > nul
+IF "%ERRORLEVEL%"=="0" (
 cd /d %~dp0
 redis-server --service-stop
 ping 192.0.2.2 -n 1 -w 3000 > nul
 GOTO MAIN
+) else (
+echo # Redis is not running.
+ping 192.0.2.2 -n 1 -w 2000 > nul
+echo Aborted.
+ping 192.0.2.2 -n 1 -w 1000 > nul
+GOTO MAIN
+)
 
 :UNINSTALL
+SC QUERY redis > NUL
+IF ERRORLEVEL 1060 (
+echo # Redis service currently not installed.
+ping 192.0.2.2 -n 1 -w 2000 > nul
+echo Aborted.
+ping 192.0.2.2 -n 1 -w 1000 > nul
+GOTO MAIN
+) else (
 cd /d %~dp0
 redis-server --service-uninstall
 ping 192.0.2.2 -n 1 -w 3000 > nul
 GOTO MAIN
+)
 
 :EOF
 echo bye..
